fea3010ab4dffb07d611b474c2ac8804b9744d6f
H9nt <63129066+h9nt@users.noreply.github.com>
2026-09-21 06:15:01 +0200

    Add Fibonacci and prime number functions

---
+++ examples/example2.js
@@ -0,0 +1,1 @@
+function fibonacci(n){if(n<=1)return n;return fibonacci(n-1)+fibonacci(n-2);}function isPrime(n){if(n<2)return false;for(var i=2;i<=Math.sqrt(n);i++){if(n%i===0)return false;}return true;}var results=[];for(var i=1;i<=20;i++){if(isPrime(i)){results.push({n:i,fib:fibonacci(i)});}}console.log(results);
